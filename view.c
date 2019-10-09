99d502a82c4f65a45261be6f079c668df69a84b3
Caroline Yumi Uehara <49196304+yumiuehara@users.noreply.github.com>
2019-10-09 10:24:59 -0300

    adicionei a funcao mostrar pecas do jogo

--- view.c
+++ view.c
@@ -70,3 +70,8 @@ char opcoesJogo()
 	printf("(3) - Organizar peças do dominó na sequência\n");
 	printf("(4) - Embaralhar peças do dominó\n");
 }
+
+void mostrarPecas()
+{
+	printf("0|0 0|1 0|2 0|3 0|4 0|5 0|6\n1|1 1|2 1|3 1|4 1|5 1|6\n2|2 2|3 2|4 2|5 2|6\n3|3 3|4 3|5 3|6\n4|4 4|5 4|6\n5|5 5|6\n6|6");
+}
