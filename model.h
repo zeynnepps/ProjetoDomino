7bf4554318069c6fd35481aef9dea5ab7a17032a
Carolina Breitenwieser <48336726+cbreitenwieser@users.noreply.github.com>
2019-10-16 09:34:13 -0300

    att do model// yumi

--- model.h
+++ model.h
@@ -1,8 +1,35 @@
 
 
-#ifndef MODEL_H_
-#define MODEL_H_
+#ifndef DOMINOMODEL_H_
+#define DOMINOMODEL_H_
+
+#include<stdbool.h>
+
+typedef struct Peca
+{
+	int ladoEsquerdo;
+	int ladoDireito;
+} tipo_Peca;
+
+//monte do dominó
+typedef struct Monte
+{
+	tipo_Peca pecas[28];
+	int qtde;
+} tipo_Monte;
+
+//conjunto de peças que cada jogador possui
+typedef struct Mao
+{
+	tipo_Peca pecas[20];
+	int qtde;
+}tipo_Mao;
 
 
-#endif /* MODEL_H_ */
+tipo_Monte criarPecas();
+tipo_Monte embaralharPecas(tipo_Monte monte);
+tipo_Mao criarMaoJogador();
+tipo_Monte distribuirPecas(tipo_Monte monte, tipo_Mao *mJ1, tipo_Mao *mJ2);
+bool comprarPeca(tipo_Monte *monte, tipo_Mao *mJ1);
 
+#endif /* DOMINOMODEL_H_ */
