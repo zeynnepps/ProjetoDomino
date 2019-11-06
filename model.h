1a0e2c53ecddf4a1b7b752b9163ee15bc3cf9d82
Caroline Yumi Uehara <49196304+yumiuehara@users.noreply.github.com>
2019-11-05 23:04:21 -0300

    Update model.h

--- model.h
+++ model.h
@@ -1,7 +1,5 @@
-
-
-#ifndef DOMINOMODEL_H_
-#define DOMINOMODEL_H_
+#ifndef MODEL_H_
+#define MODEL_H_
 
 #include<stdbool.h>
 
@@ -25,11 +23,20 @@ typedef struct Mao
 	int qtde;
 }tipo_Mao;
 
+//a mesa do jogo
+typedef struct Mesa
+{
+	tipo_Peca pecas[28];
+	int qtde;
+}tipo_Mesa;
+
 
 tipo_Monte criarPecas();
 tipo_Monte embaralharPecas(tipo_Monte monte);
 tipo_Mao criarMaoJogador();
 tipo_Monte distribuirPecas(tipo_Monte monte, tipo_Mao *mJ1, tipo_Mao *mJ2);
 bool comprarPeca(tipo_Monte *monte, tipo_Mao *mJ1);
+void organizarPecas(tipo_Monte* monte, tipo_Mao* mJ1, tipo_Mao* mJ2);
+bool buscarPrimeiroJogador(tipo_Mao* M1, tipo_Mao* M2);
 
-#endif /* DOMINOMODEL_H_ */
+#endif /* MODEL_H_ */
