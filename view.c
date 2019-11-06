e424efcbeb37613753bb2cd353816e45103f43d7
Caroline Yumi Uehara <49196304+yumiuehara@users.noreply.github.com>
2019-11-05 23:05:51 -0300

    Update view.c

--- view.c
+++ view.c
@@ -3,16 +3,12 @@ PROJETO DO SEGUNDO SEMESTRE DE CIÊNCIA DA COMPUTAÇÃO
 NOMES:   CAROLINA BREITENWIESER RA00222650
          CAROLINE UEHARA  RA00
          ZEYNEP SALIHOGLU RA00  
-         
-         
-=======VIEW======
-MENU
-PEÇAS 
 */     
 #include <stdio.h>
 #include <string.h>
 #include <ctype.h>
 #include "dominoView.h"
+#include "dominoModel.h"
 
 //menu de quando o jogo é aberto
 char exibirMenu()
@@ -59,10 +55,8 @@ char opcoesJogo()
 
 	printf("OPÇÕES DO JOGO\n");
 	printf("(1) - Mostrar peças do dominó\n");
-	printf("(2) - Organizar peças do dominó na sequência\n"); //no inicio do jogos as peças sâo embaralhadas automaticamente
-	printf("(3) - Embaralhar peças do dominó\n");
-	printf("(4) - Voltar ao Menu\n");
-    printf("(5) - Salvar jogo\n");
+	printf("(2) - Regras do dominó\n");
+	printf("(3) - Voltar ao menu\n");
 	printf("Selecione a opção desejada: ");
 	opcao = getchar();
 	flush_in();
@@ -81,4 +75,48 @@ void mostrarPecas()
 	printf("PEÇAS DO DOMINÓ\n");
 	printf("0|0 0|1 0|2 0|3 0|4 0|5 0|6\n1|1 1|2 1|3 1|4 1|5 1|6\n2|2 2|3 2|4 2|5 2|6\n3|3 3|4 3|5 3|6\n4|4 4|5 4|6\n5|5 5|6\n6|6\n");
 	printf("\n");
+}
+
+void mostrarMao(tipo_Mao mJ1)
+{
+	int i;
+
+	printf("**PEÇAS**\n");
+	for (i = 0; i < mJ1.qtde; i++)
+	{
+		printf("(%d)%d", (i+1), mJ1.pecas[i].ladoEsquerdo);
+		printf("%d\t",mJ1.pecas[i].ladoDireito);
+	}
+
+	printf("\n");
+}
+
+void regrasDomino()
+{
+	printf("REGRAS DO DOMINÓ\n");
+	printf("O monte é embaralhado e cada jogador recebe 7 peças.\n");
+	printf("Começa o jogo quem tiver a peça 66 ou a maior peça dupla.\nSe ninguém possuir peças duplas, então o jogador que possuir a menor soma de suas peças começa.\n");
+	printf("O jogador deve jogar uma peça que possua o mesmo número da que está na mesa.\n");
+	printf("Quando o jogador não possuir peça para jogar, ele deve comprar até encontrar uma que possa ser jogada.\n");
+	printf("Vence o jogador que jogar todas suas peças primeiro.\n");
+	printf("\n");
+}
+
+void mostrarMesa(tipo_Mesa mesa)
+{
+	int i;
+	
+	if (mesa.qtde > 0)
+	{
+		for(i=0; i<mesa.qtde; i++)
+		{
+			printf("-%d%d-",mesa.pecas[i].ladoEsquerdo,mesa.pecas[i].ladoDireito);
+		}
+	}
+	else
+	{
+		printf("Mesa vazia!\n");//talvez nao seja necessario, checar depois!!
+	}
+
+	
 }
