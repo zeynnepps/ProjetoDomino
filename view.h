f41f263eb45de2ecbdc5fb3b4d13b05a1166abea
Caroline Yumi Uehara <49196304+yumiuehara@users.noreply.github.com>
2019-11-05 23:07:37 -0300

    Update view.h

--- view.h
+++ view.h
@@ -4,8 +4,11 @@
  *  Created on: 3 de out de 2019
  *      Author: yumi
  */
-#ifndef DOMINOVIEW_H_
-#define DOMINOVIEW_H_
+
+#ifndef VIEW_H_
+#define VIEW_H_
+
+#include "model.h"
 
 char exibirMenu();
 void flush_in();
@@ -13,5 +16,8 @@ char determinarQtdJogador();
 char opcoesJogo();
 void mostrarMsg(char* msg);
 void mostrarPecas();
+void mostrarMao(tipo_Mao mJ1);
+void regrasDomino();
+void mostrarMesa(tipo_Mesa mesa);
 
-#endif /* VIEW_H_ */
+#endif /* DOMINOVIEW_H_ */
